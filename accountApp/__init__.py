9f039ed94f194dcb7ffb46cd791c36d599cffc76
DESKTOP-KKKOTUV\ojh789 <ojh789ojh@naver.com>
2021-09-02 12:24:36 +0900

    no message

--- accountApp/__init__.py
+++ accountApp/__init__.py
@@ -7,3 +7,20 @@ test"""
 # python manage.py startapp profileapp
 # pip install pillow
 # python manage.py startapp articleapp
+# ssh -i gis_test_1.pem ubuntu@3.16.67.10
+
+# docker
+# sudo apt-get update
+# sudo apt-get install \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     gnupg \
+#     lsb-release
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#  echo \
+#   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt-get update
+#  sudo apt-get install docker-ce docker-ce-cli containerd.io
+#  sudo docker run hello-world
